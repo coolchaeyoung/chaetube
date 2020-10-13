ab5959bb04f181c0e9edc24ed35a8db2598895f3
coolchaeyoung <ggodchae@naver.com>
2020-10-13 18:49:22 +0900

    add db model

--- init.js
+++ init.js
@@ -1,4 +1,8 @@
+import "./db";
 import app from "./app";
-const PORT = 4000;
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
