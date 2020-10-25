2f8f46c0d628142951fd576cee1fe17d0b784ba9
coolchaeyoung <ggodchae@naver.com>
2020-10-25 16:26:15 +0900

    volume btn

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,6 +1,7 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
 const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeBtn");
 
 const handlePlayBtn = () => {
     if(videoPlayer.paused) {
@@ -11,9 +12,20 @@ const handlePlayBtn = () => {
         playBtn.innerHTML = '<i class="fas fa-play"></i>';
     }
 }
- 
+
+const handleVolumeClick = () => {
+    if (videoPlayer.muted) {
+        videoPlayer.muted = false;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    } else {
+        videoPlayer.muted = true;
+        volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
+};
+
 const init = () => {
     playBtn.addEventListener("click", handlePlayBtn);
+    volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if(videoContainer) {
